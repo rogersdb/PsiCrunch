13340eb75e2212389310cb82a80b65ad35c11856
Brian Rogers <69013849+rogersdb@users.noreply.github.com>
2025-08-25 19:28:38 -0700

    Extend potential Jacobian for multi-plane model

--- Source/jacpotential.F90
+++ Source/jacpotential.F90
@@ -1,47 +1,31 @@
 !!! *** Copyright Notice ***
-!!! “CrunchFlow”, Copyright (c) 2016, The Regents of the University of California, through Lawrence Berkeley National Laboratory 
-!!! (subject to receipt of any required approvals from the U.S. Dept. of Energy).  All rights reserved.
-!!! 
-!!! If you have questions about your rights to use or distribute this software, please contact 
-!!! Berkeley Lab's Innovation & Partnerships Office at  IPO@lbl.gov.
-!!! 
-!!! NOTICE.  This Software was developed under funding from the U.S. Department of Energy and the U.S. Government 
-!!! consequently retains certain rights. As such, the U.S. Government has been granted for itself and others acting 
-!!! on its behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software to reproduce, distribute copies to the public, 
-!!! prepare derivative works, and perform publicly and display publicly, and to permit other to do so.
+!!! CrunchFlow, Copyright (c) 2016, The Regents of the University of California,
+!!! through Lawrence Berkeley National Laboratory. All rights reserved.
 !!!
-!!! *** License Agreement ***
-!!! “CrunchFlow”, Copyright (c) 2016, The Regents of the University of California, through Lawrence Berkeley National Laboratory)
-!!! subject to receipt of any required approvals from the U.S. Dept. of Energy).  All rights reserved."
-!!! 
-!!! Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-!!! 
-!!! (1) Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+!!! Redistribution and use in source and binary forms, with or without
+!!! modification, are permitted provided that the following conditions are met:
+!!! (1) Redistributions of source code must retain the above copyright notice,
+!!!     this list of conditions and the following disclaimer.
+!!! (2) Redistributions in binary form must reproduce the above copyright notice,
+!!!     this list of conditions and the following disclaimer in the documentation
+!!!     and/or other materials provided with the distribution.
+!!! (3) Neither the name of the University of California, Lawrence Berkeley
+!!!     National Laboratory, U.S. Dept. of Energy nor the names of its
+!!!     contributors may be used to endorse or promote products derived from
+!!!     this software without specific prior written permission.
 !!!
-!!! (2) Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
-!!! in the documentation and/or other materials provided with the distribution.
-!!!
-!!! (3) Neither the name of the University of California, Lawrence Berkeley National Laboratory, U.S. Dept. of Energy nor the names of 
-!!! its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-!!!
-!!! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
-!!! BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
-!!! SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-!!! DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-!!! OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-!!! LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF 
-!!! THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-!!!
-!!! You are under no obligation whatsoever to provide any bug fixes, patches, or upgrades to the features, functionality or 
-!!! performance of the source code ("Enhancements") to anyone; however, if you choose to make your
-!!! Enhancements available either publicly, or directly to Lawrence Berkeley National Laboratory, without 
-!!! imposing a separate written license agreement for such 
-!!! Enhancements, then you hereby grant the following license: a  non-exclusive, royalty-free perpetual license to install, use, 
-!!! modify, prepare derivative works, incorporate into other computer software, distribute, and sublicense such enhancements or 
-!!! derivative works thereof, in binary and source code form.
+!!! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+!!! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+!!! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+!!! ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+!!! LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+!!! CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+!!! SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+!!! INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+!!! CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+!!! ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+!!! POSSIBILITY OF SUCH DAMAGE.
 
-!!!      ****************************************
-    
 SUBROUTINE jacpotential(ncomp,nsurf,nsurf_sec,npot,nx,ny,nz)
 USE crunchtype
 USE concentration
@@ -51,76 +35,79 @@ USE mineral
 IMPLICIT NONE
 
 !  External variables and arrays
-
-INTEGER(I4B), INTENT(IN)                                                :: ncomp
-INTEGER(I4B), INTENT(IN)                                                :: nsurf
-INTEGER(I4B), INTENT(IN)                                                :: nsurf_sec
-INTEGER(I4B), INTENT(IN)                                                :: npot
-INTEGER(I4B), INTENT(IN)                                                :: nx
-INTEGER(I4B), INTENT(IN)                                                :: ny
-INTEGER(I4B), INTENT(IN)                                                :: nz
+INTEGER(I4B), INTENT(IN) :: ncomp
+INTEGER(I4B), INTENT(IN) :: nsurf
+INTEGER(I4B), INTENT(IN) :: nsurf_sec
+INTEGER(I4B), INTENT(IN) :: npot
+INTEGER(I4B), INTENT(IN) :: nx
+INTEGER(I4B), INTENT(IN) :: ny
+INTEGER(I4B), INTENT(IN) :: nz
 
 !  Internal variables and arrays
+INTEGER(I4B) :: jx, jy, jz
+INTEGER(I4B) :: npt2, i, is, ns
+INTEGER(I4B) :: idxR0, idxRb, idxRd, k
+REAL(DP)    :: surfconc, correct, term1
+REAL(DP)    :: sum0, sumb
 
-REAL(DP)                                                                :: delta_z
-REAL(DP)                                                                :: surfconc
-REAL(DP)                                                                :: mutemp
-REAL(DP)                                                                :: sum
-
-INTEGER(I4B)                                                            :: npt2
-INTEGER(I4B)                                                            :: is
-INTEGER(I4B)                                                            :: is2
-INTEGER(I4B)                                                            :: ns
-INTEGER(I4B)                                                            :: i
-INTEGER(I4B)                                                            :: jx
-INTEGER(I4B)                                                            :: jy
-INTEGER(I4B)                                                            :: jz
-
-fjpotncomp = 0.0
-fjpotnsurf = 0.0
+fjpotncomp = 0.0d0
+fjpotnsurf = 0.0d0
 
 DO jz = 1,nz
   DO jy = 1,ny
     DO jx = 1,nx
 
-      DO ns = 1,nsurf_sec
-        
-        surfconc = spsurf10(ns+nsurf,jx,jy,jz)
-        delta_z = zsurf(ns+nsurf) - zsurf(islink(ns))
+      DO npt2 = 1,npot
+
+        k = ksurf(ispot(npt2))
+        correct = wtmin(k)*specificByGrid(k,jx,jy,jz)*volfx(k,jx,jy,jz)/volmol(k)
+        term1 = 96485.0d0/correct
+
+        idxR0 = npt2
+        idxRb = npt2 + npot
+        idxRd = npt2 + 2*npot
 
         DO i = 1,ncomp
-          IF (musurf(ns,i) /= 0.0) THEN
-            mutemp = musurf(ns,i)
-            DO npt2 = 1,npot
-             IF (ksurf(islink(ns)) == kpot(npt2)) THEN
-                fjpotncomp(npt2,i,jx,jy,jz) = fjpotncomp(npt2,i,jx,jy,jz) -         &
-                    2.0*delta_z*mutemp*surfconc
-              END IF
-            END DO     
-          END IF
-        END DO       
-        
-!!!  Surface Complexation Cheat Sheet    
-!!!    kPotential(k) --> Logical to EDL potential
-!!!    ksurf(is) --> pointer for primary nsurf complex to mineral (initialized in read_surface.F90)
-!!!    iedl(is) --> 0 for electrostatic, 1 for -no_edl
-!!!    npot --> number of potentials
-!!!    kpot(npt) --> pointer to mineral upon which the potential is developed
-!!!    islink(ns) --> pointer from secondary surface complex (ns) to primary surface complex (is)
-!!!    ksurf(islink(ns)) --> This would point from a secondary surface complex (ns) to a primary (islink(ns)) complex to a mineral
-!!!    nptlink(ns) --> pointer of surface complex (primary or secondary) to potential (npt)
+          sum0 = 0.0d0
+          sumb = 0.0d0
+          DO ns = 1,nsurf_sec
+            IF (ksurf(islink(ns)) == kpot(npt2)) THEN
+              surfconc = spsurf10(ns+nsurf,jx,jy,jz)
+              sum0 = sum0 - musurf(ns,i)*z0_s(ns+nsurf)*surfconc*term1
+              sumb = sumb - musurf(ns,i)*zb_s(ns+nsurf)*surfconc*term1
+            END IF
+          END DO
+          fjpotncomp(idxR0,i,jx,jy,jz) = sum0
+          fjpotncomp(idxRb,i,jx,jy,jz) = sumb
+          fjpotncomp(idxRd,i,jx,jy,jz) = 0.0d0
+        END DO
 
         DO is = 1,nsurf
-          IF (musurf(ns,is+ncomp) /= 0.0) THEN
-            mutemp = musurf(ns,is+ncomp)
-            DO npt2 = 1,npot
-              IF (ksurf(islink(ns)) == kpot(npt2)) THEN
-                fjpotnsurf(npt2,is,jx,jy,jz) = fjpotnsurf(npt2,is,jx,jy,jz) -       & 
-                   2.0*delta_z*mutemp*surfconc
-              END IF
-            END DO
-          END IF   
+          sum0 = 0.0d0
+          sumb = 0.0d0
+          DO ns = 1,nsurf_sec
+            IF (ksurf(islink(ns)) == kpot(npt2)) THEN
+              surfconc = spsurf10(ns+nsurf,jx,jy,jz)
+              sum0 = sum0 - musurf(ns,is+ncomp)*z0_s(ns+nsurf)*surfconc*term1
+              sumb = sumb - musurf(ns,is+ncomp)*zb_s(ns+nsurf)*surfconc*term1
+            END IF
+          END DO
+          IF (ksurf(is) == kpot(npt2)) THEN
+            sum0 = sum0 - z0_s(is)*spsurf10(is,jx,jy,jz)*term1
+            sumb = sumb - zb_s(is)*spsurf10(is,jx,jy,jz)*term1
+          END IF
+          fjpotnsurf(idxR0,is,jx,jy,jz) = sum0
+          fjpotnsurf(idxRb,is,jx,jy,jz) = sumb
+          fjpotnsurf(idxRd,is,jx,jy,jz) = 0.0d0
         END DO
+
+        IF (C2(npt2) == 0.0d0) THEN
+          fjpotncomp(idxRb,:,jx,jy,jz) = 0.0d0
+          fjpotnsurf(idxRb,:,jx,jy,jz) = 0.0d0
+          fjpotncomp(idxRd,:,jx,jy,jz) = fjpotncomp(idxR0,:,jx,jy,jz)
+          fjpotnsurf(idxRd,:,jx,jy,jz) = fjpotnsurf(idxR0,:,jx,jy,jz)
+        END IF
+
       END DO
 
     END DO
@@ -129,33 +116,3 @@ END DO
 
 RETURN
 END SUBROUTINE jacpotential
-  
-!!!    DO i = 1,ncomp
-!!!        DO npt2 = 1,npot
-!!!          is2 = ispot(npt2)
-!!!          sum = 0.0
-!!!          DO ns = 1,nsurf_sec
-!!!            delta_z = zsurf(ns+nsurf) - zsurf(islink(ns))
-!!!            IF (islink(ns) == is2) THEN
-!!!              sum = sum - 2.0*delta_z*musurf(ns,i)*spsurf10(ns+nsurf,jx,jy,jz)
-!!!            END IF
-!!!          END DO
-!!!          fjpotncomp(npt2,i,jx,jy,jz) = sum     
-!!!        END DO
-!!!    END DO
-
-!!!    DO is = 1,nsurf
-!!!      DO npt2 = 1,npot
-!!!        is2 = ispot(npt2)
-!!!        sum = 0.0
-!!!        IF (is == ispot(npt2)) THEN
-!!!          DO ns = 1,nsurf_sec
-!!!            delta_z = zsurf(ns+nsurf) - zsurf(islink(ns))
-!!!            IF (islink(ns) == is2) THEN
-!!!              sum = sum - 2.0*delta_z*musurf(ns,is+ncomp)*spsurf10(ns+nsurf,jx,jy,jz)
-!!!            END IF
-!!!          END DO
-!!!          fjpotnsurf(npt2,is,jx,jy,jz) = sum
-!!!        END IF   
-!!!      END DO
-!!!    END DO
